0de7b0ce1182ea0f4e38a85b24f6d832302c039a
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2023-08-23 14:17:14 +0200

    re-read token from file when expired (#71)
    * re-read token from file when expired
    
    * rename file

--- radix/api_server.go
+++ radix/api_server.go
@@ -4,7 +4,7 @@ import "github.com/equinor/radix-github-webhook/models"
 
 // APIServer Stub methods in order to mock endpoints
 type APIServer interface {
-	ShowApplications(bearerToken, sshURL string) ([]*models.ApplicationSummary, error)
-	GetApplication(bearerToken, appName string) (*models.Application, error)
-	TriggerPipeline(bearerToken, appName, branch, commitID, triggeredBy string) (*models.JobSummary, error)
+	ShowApplications(sshURL string) ([]*models.ApplicationSummary, error)
+	GetApplication(appName string) (*models.Application, error)
+	TriggerPipeline(appName, branch, commitID, triggeredBy string) (*models.JobSummary, error)
 }
